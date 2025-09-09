10b1a27ae242a8ec306afcf304f014209dc3626f
6312999 <6312999@edu.vaniercollege.qc.ca>
2025-09-09 16:49:30 -0400

    Add files via upload

---
+++ Lab 2.py
@@ -0,0 +1,29 @@
+
+
+
+# Function 1: Cal
+def calculate_car_distance(t):
+    speed=20
+    result=speed*time  
+    return result
+time=(float(input("Enter the time") ))                        #Have it put it's own time
+print(f"What is the distance the car will travel", {calculate_car_distance (time)}) 
+
+
+
+# Function 1: Calculate the height of the ball after time t
+# This function should take the initial height h0 and time t as inputs, and return the height at time t.
+def calculate_height(h0, t):
+    g= 9.8
+    result=h0-1/2*g*t**2  
+    return result
+
+height=(float(input("Enter your height")))                    #Have it put it's own height
+time=(float(input("Enter the time") ))                        #Have it put it's own time
+print(f"What is the height", {calculate_height (height,time)}) 
+height=(float(input("Enter your height")))                    #Have it put it's own height
+time=(float(input("Enter the time") ))                        #Have it put it's own time
+print(f"What is the height", {calculate_height (height,time)}) 
+height=(float(input("Enter your height")))                    #Have it put it's own height
+time=(float(input("Enter the time") ))                        #Have it put it's own time
+print(f"What is the height", {calculate_height (height,time)}) 
